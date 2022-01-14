516348a5a9fa0fe328923a6cc9ccb4787661ad56
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-14 12:23:16 -0600

    commit tree b285ff912ea7fbae518c52a32f1ea24c42503f5f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(array){
+
+    const findelem = array.find(elem => elem.result === 'W')
+    console.log(findelem);
+
+    if(findelem != undefined){
+        return findelem.year;
+    }
+    else{
+        return undefined;
+    }
+    
+    
+
+}
